3afad9cc5d63038b177ff529676a7d3360386157
HOOOD1N1 <44617359+HOOOD1N1@users.noreply.github.com>
2022-02-10 01:32:08 +0200

    Added Initial Project

---
+++ src/main/java/com/springframework/edypetclinic/EdyPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.edypetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdyPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdyPetClinicApplication.class, args);
+    }
+
+}
