411d8d377b3598058f91ead4e4a44e53ee42e2d5
HOOOD1N1 <44617359+HOOOD1N1@users.noreply.github.com>
2022-02-14 08:49:51 +0200

    Added PetTypeService

---
+++ pet-clinic-data/src/main/java/com/springframework/edypetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.springframework.edypetclinic.services;
+
+import com.springframework.edypetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
