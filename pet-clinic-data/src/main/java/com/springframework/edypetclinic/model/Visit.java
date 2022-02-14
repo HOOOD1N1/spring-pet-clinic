be9731341dbf5a0d583332106f8dd4d10c49e40e
HOOOD1N1 <44617359+HOOOD1N1@users.noreply.github.com>
2022-02-14 10:43:31 +0200

    Updated Visits and Pets to JPA Entities

--- pet-clinic-data/src/main/java/com/springframework/edypetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/springframework/edypetclinic/model/Visit.java
@@ -1,11 +1,21 @@
 package com.springframework.edypetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name="visits")
 public class Visit extends BaseEntity {
 
+    @Column(name="date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name="pet_id")
+    private Pet pet;
 
     public LocalDate getDate() {
         return date;
@@ -30,7 +40,5 @@ public class Visit extends BaseEntity {
     public void setPet(Pet pet) {
         this.pet = pet;
     }
-
-    private Pet pet;
 
 }
