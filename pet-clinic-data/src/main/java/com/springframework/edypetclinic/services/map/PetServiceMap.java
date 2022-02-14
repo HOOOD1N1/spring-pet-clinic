cda3fbf8be730da307f9fae78dacde7f63858b4d
HOOOD1N1 <44617359+HOOOD1N1@users.noreply.github.com>
2022-02-14 04:25:04 +0200

    Add Owners Controller and Index Page

--- pet-clinic-data/src/main/java/com/springframework/edypetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/springframework/edypetclinic/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package com.springframework.edypetclinic.services.map;
 
 import com.springframework.edypetclinic.model.Pet;
-import com.springframework.edypetclinic.services.CrudService;
+import com.springframework.edypetclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
---
+++ pet-clinic-web/src/main/java/com/springframework/edypetclinic/bootstrap/DataLoader.java
@@ -0,0 +1,56 @@
+package com.springframework.edypetclinic.bootstrap;
+
+import com.springframework.edypetclinic.model.Owner;
+import com.springframework.edypetclinic.model.Vet;
+import com.springframework.edypetclinic.services.OwnerService;
+import com.springframework.edypetclinic.services.VetService;
+import com.springframework.edypetclinic.services.map.OwnerServiceMap;
+import com.springframework.edypetclinic.services.map.VetServiceMap;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+
+
+    public DataLoader(){
+    ownerService = new OwnerServiceMap();
+    vetService = new VetServiceMap();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+    Owner owner1 = new Owner();
+    owner1.setId(1L);
+    owner1.setFirstName("edy");
+    owner1.setLastName("d");
+    ownerService.save(owner1);
+
+    Owner owner2 = new Owner();
+    owner2.setId(1L);
+    owner2.setFirstName("ra");
+    owner2.setLastName("hom");
+    ownerService.save(owner2);
+
+        System.out.println("Loading Owners....");
+
+    Vet vet1 = new Vet();
+    vet1.setId(1L);
+    vet1.setFirstName("Chris");
+    vet1.setLastName("Don");
+    vetService.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet2.setId(1L);
+        vet2.setFirstName("Hall");
+        vet2.setLastName("Ham");
+        vetService.save(vet2);
+
+
+        System.out.println("Loading Vets....");
+    }
+}
