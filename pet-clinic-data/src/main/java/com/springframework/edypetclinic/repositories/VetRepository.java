f1db3528ff56a91c05f4458e974a2dc346bafb7b
HOOOD1N1 <44617359+HOOOD1N1@users.noreply.github.com>
2022-02-14 12:11:19 +0200

    Added Spring Data JPA Services

--- pet-clinic-data/src/main/java/com/springframework/edypetclinic/repositories/VetRepository.java
+++ pet-clinic-data/src/main/java/com/springframework/edypetclinic/repositories/VetRepository.java
@@ -1,6 +1,7 @@
 package com.springframework.edypetclinic.repositories;
 
+import com.springframework.edypetclinic.model.Vet;
 import org.springframework.data.repository.CrudRepository;
 
-public interface VetRepository extends CrudRepository<VetRepository, Long> {
+public interface VetRepository extends CrudRepository<Vet, Long> {
 }
